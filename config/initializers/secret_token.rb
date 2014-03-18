2f6facc1bf23201ae012030870cf60ba9f104080
Lindsey Sugino <lindseysugino@gmail.com>
2014-03-17 17:39:14 -0700

    rails skeleton with postgresql + rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProgrammingAnalogies::Application.config.secret_key_base = '2cbdf4676a97e5b0b8b21097be33a56303ccf9d7862d2193ca5c995fc99da02786229a8e2b6d60ee01bf3dbb30b47e66b5b6c0e790f88442afab6ad63dfc9269'
